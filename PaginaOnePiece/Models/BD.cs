de46b5ac9875c0740520a2c0362026c0737547e0
FRANCO KALISZCZAK BENITEZ <46916289@ort>
2022-10-25 08:21:20 -0300

    gitignore

--- PaginaOnePiece/Models/BD.cs
+++ PaginaOnePiece/Models/BD.cs
@@ -13,5 +13,28 @@ namespace PaginaOnePiece
         private static List<Temporada> _ListaTemporadas = new List<Temporada>();
         private static List<Mar> _ListaMares = new List<Mar>();
         private static List<Personaje> _ListaPersonajes = new List<Personaje>();
+
+
+        public static List<Temporada> ListarTemporadasByID(int IdMar)
+        {
+             using(SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT * FROM Temporada WHERE IdMar = @pIdMar";
+                _ListaTemporadas = db.Query<Actores>(sql, new {pIdMar = IdMar}).ToList();
+            }
+            return _ListaTemporadas;
+        }
+
+        public static List<Personaje> ListarPersonajes()
+        {
+             using(SqlConnection db = new SqlConnection(_connectionString))
+            {
+                string sql = "SELECT * FROM Personaje";
+                _ListaPersonajes = db.Query<Personaje>(sql).ToList();
+            }
+            return _ListaPersonajes;
+        }
+
     }
+    
 }
